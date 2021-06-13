98d09a7b5f7494329fe8ea4a9ab3a15aaa695df2
Elijardh <elijardh@gmail.com>
2021-06-13 16:35:27 +0100

    image upload test

---
+++ lib/Presentation/Pages/upload_page/uploadpage.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:zia/utils/colors.dart';
+import 'package:zia/utils/size_config.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
+
+class UploadPage extends StatefulWidget {
+  @override
+  _UploadPageState createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  PickedFile _fileImage;
+  SizeConfig config = SizeConfig();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(
+              Icons.close_fullscreen_outlined,
+              color: XColors.primaryColor,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: GestureDetector(
+              onTap: () {
+                setState(() async {
+                  _fileImage = await ImagePicker.platform
+                      .pickImage(source: ImageSource.gallery);
+                });
+              },
+              child: Container(
+                  child: _fileImage != null
+                      ? Image.file(
+                          File(_fileImage.path),
+                          fit: BoxFit.fill,
+                        )
+                      : Center(
+                          child: Text("No Image"),
+                        ))),
+        ));
+  }
+}
