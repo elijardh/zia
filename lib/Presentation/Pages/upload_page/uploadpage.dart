675e464c16ead5e9429da80773b2aed6e000b416
Elijardh <elijardh@gmail.com>
2021-06-14 06:32:07 +0100

    image upload test

--- lib/Presentation/Pages/upload_page/uploadpage.dart
+++ lib/Presentation/Pages/upload_page/uploadpage.dart
@@ -6,6 +6,7 @@ import 'package:zia/utils/colors.dart';
 import 'package:zia/utils/size_config.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
+import 'package:zia/widgets/text_field.dart';
 import 'package:zia/widgets/texts.dart';
 import 'package:zia/widgets/y_margin.dart';
 import 'package:provider/provider.dart';
@@ -16,12 +17,28 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  TextEditingController productName = TextEditingController();
+  TextEditingController productCost = TextEditingController();
+  TextEditingController productAmount = TextEditingController();
+  TextEditingController productDescription = TextEditingController();
+
+  String category;
   PickedFile _fileImage;
   SizeConfig config = SizeConfig();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
+        bottomSheet: XButton(
+          onClick: () => navigate(context, CartPage()),
+          text: "Upload Product",
+          textColor: Colors.white,
+          height: config.sh(50),
+          width: SizeConfig.screenWidthDp,
+          buttonColor: XColors.primaryColor,
+          radius: 0,
+        ),
         appBar: AppBar(
           backgroundColor: Colors.white,
           leading: IconButton(
@@ -74,25 +91,91 @@ class _UploadPageState extends State<UploadPage> {
                 fontWeight: FontWeight.bold,
                 textColor: Colors.black,
               ),
+              Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NormalText(
+                      text: "Product Name",
+                      textColor: Colors.black,
+                    ),
+                    EUnderLineButton(
+                      controller: productName,
+                      hintText: "Product Name",
+                      hintTextColor: Colors.black,
+                      fillColor: XColors.primaryColor.withOpacity(0.2),
+                      enabledBorderColor: XColors.primaryColor,
+                      focusedBorderColor: XColors.primaryColor,
+                    ),
+                    YMargin(50),
+                    NormalText(
+                      text: "Price",
+                      textColor: Colors.black,
+                    ),
+                    EUnderLineButton(
+                      controller: productCost,
+                      hintText: "Price",
+                      hintTextColor: Colors.black,
+                      fillColor: XColors.primaryColor.withOpacity(0.2),
+                      enabledBorderColor: XColors.primaryColor,
+                      focusedBorderColor: XColors.primaryColor,
+                    ),
+                    YMargin(50),
+                    NormalText(
+                      text: "Amount in Stock",
+                      textColor: Colors.black,
+                    ),
+                    EUnderLineButton(
+                      controller: productAmount,
+                      hintText: "How much of the product do you have",
+                      hintTextColor: Colors.black,
+                      fillColor: XColors.primaryColor.withOpacity(0.2),
+                      enabledBorderColor: XColors.primaryColor,
+                      focusedBorderColor: XColors.primaryColor,
+                    ),
+                    NormalText(
+                      text: "Category",
+                      textColor: Colors.black,
+                    ),
+                    DropdownButton<String>(
+                      elevation: 0,
+                      value: "Select Category",
+                      icon: const Icon(Icons.arrow_downward),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          category = value;
+                        });
+                      },
+                      items: <String>[
+                        'Electronics',
+                        'Jewelery',
+                        "Men's Clothing",
+                        "Women's Clothing"
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    YMargin(50),
+                    Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: XColors.primaryColor.withOpacity(0.2),
+                          labelText: "Describe the product",
+                        ),
+                        controller: productDescription,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
-        ) /*GestureDetector(
-              onTap: () async {
-                PickedFile test = await ImagePicker.platform
-                    .pickImage(source: ImageSource.gallery);
-                setState(() {
-                  _fileImage = test;
-                });
-              },
-              child: Container(
-                  child: _fileImage != null
-                      ? Image.file(
-                          File(_fileImage.path),
-                          fit: BoxFit.fill,
-                        )
-                      : Center(
-                          child: Text("No Image"),
-                        ))),*/
-            ));
+        )));
   }
 }
